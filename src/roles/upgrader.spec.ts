58c9854f91e0c3c1bf3a67a605f5d6e14f226e3b
eduter <emterroso@gmail.com>
2021-04-09 00:06:39 +0200

    Improve upgrader's spec

--- src/roles/upgrader.spec.ts
+++ src/roles/upgrader.spec.ts
@@ -4,21 +4,17 @@ import roleUpgrader, { Upgrader } from './upgrader';
 const controller = mockStructure(STRUCTURE_CONTROLLER);
 const source1 = mockInstanceOf<Source>({ id: 'source1' as Id<Source> });
 const source2 = mockInstanceOf<Source>({ id: 'source2' as Id<Source> });
-const room = mockInstanceOf<Room>({
-  controller,
-  find: (type: FindConstant) => (type === FIND_SOURCES ? [source1, source2] : [])
-});
 
 describe('Upgrader role', () => {
 
   it('upgrades the controller, when it has energy and is within range', () => {
     const creep = mockInstanceOf<Upgrader>({
+      store: { energy: 50 },
       memory: {
         role: 'upgrader',
         upgrading: true
       },
-      room,
-      store: { energy: 50 },
+      room: { controller },
       upgradeController: () => OK
     });
 
@@ -29,14 +25,14 @@ describe('Upgrader role', () => {
 
   it('moves towards controller, when it has energy but is out of range', () => {
     const creep = mockInstanceOf<Upgrader>({
+      store: { energy: 50 },
       memory: {
         role: 'upgrader',
         upgrading: true
       },
-      moveTo: () => OK,
-      room,
-      store: { energy: 50 },
-      upgradeController: () => ERR_NOT_IN_RANGE
+      room: { controller },
+      upgradeController: () => ERR_NOT_IN_RANGE,
+      moveTo: () => OK
     });
 
     roleUpgrader.run(creep);
@@ -47,46 +43,48 @@ describe('Upgrader role', () => {
 
   it("harvests, when it's near a source and not full", () => {
     const creep = mockInstanceOf<Upgrader>({
-      harvest: () => OK,
+      store: { getFreeCapacity: () => 50 },
       memory: {
         role: 'upgrader',
         upgrading: false
       },
-      room,
-      store: { getFreeCapacity: () => 50 }
+      room: { find: () => [source1, source2] },
+      harvest: () => OK
     });
 
     roleUpgrader.run(creep);
     expect(creep.memory.upgrading).toBeFalsy();
+    expect(creep.room.find).toHaveBeenCalledWith(FIND_SOURCES);
     expect(creep.harvest).toHaveBeenCalledWith(source1);
   });
 
   it("moves to a source, when it's not full and not near a source", () => {
     const creep = mockInstanceOf<Upgrader>({
-      harvest: () => ERR_NOT_IN_RANGE,
+      store: { getFreeCapacity: () => 50 },
       memory: {
         role: 'upgrader',
         upgrading: false
       },
-      moveTo: () => OK,
-      room,
-      store: { getFreeCapacity: () => 50 }
+      room: { find: () => [source1, source2] },
+      harvest: () => ERR_NOT_IN_RANGE,
+      moveTo: () => OK
     });
     roleUpgrader.run(creep);
     expect(creep.memory.upgrading).toBeFalsy();
+    expect(creep.room.find).toHaveBeenCalledWith(FIND_SOURCES);
     expect(creep.moveTo).toHaveBeenCalledWith(source1, expect.anything());
   });
 
   it('switches to upgrading when it gets full', () => {
     const creep = mockInstanceOf<Upgrader>({
+      store: { getFreeCapacity: () => 0 },
       memory: {
         role: 'upgrader',
         upgrading: false
       },
-      room,
-      say: () => OK,
-      store: { getFreeCapacity: () => 0 },
-      upgradeController: () => OK
+      room: { controller },
+      upgradeController: () => OK,
+      say: () => OK
     });
     roleUpgrader.run(creep);
     expect(creep.memory.upgrading).toBeTruthy();
@@ -94,19 +92,20 @@ describe('Upgrader role', () => {
 
   it('switches to harvesting when it gets empty', () => {
     const creep = mockInstanceOf<Upgrader>({
-      harvest: () => OK,
+      store: {
+        energy: 0,
+        getFreeCapacity: () => 50
+      },
       memory: {
         role: 'upgrader',
         upgrading: true
       },
-      room,
-      say: () => OK,
-      store: {
-        energy: 0,
-        getFreeCapacity: () => 50
-      }
+      room: { find: () => [source1, source2] },
+      harvest: () => OK,
+      say: () => OK
     });
     roleUpgrader.run(creep);
+    expect(creep.room.find).toHaveBeenCalledWith(FIND_SOURCES);
     expect(creep.memory.upgrading).toBeFalsy();
   });
 
